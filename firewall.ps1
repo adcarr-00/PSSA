a1dd9777329fac3d312aee49134ac94c2e4a9f5f
adcarr-00 <adam.carr@nationwide.co.uk>
2024-11-19 16:35:51 +0000

    Add files via upload

---
+++ firewall.ps1
@@ -0,0 +1,5 @@
+echo 'This is a Test to check that the PS script does not open vulnerable ports inbound'
+New-NetFirewallRule -DisplayName 'Open Telnet' -Direction Inbound -LocalPort 23 -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName 'Open FTP' -Direction Inbound -LocalPort 20 -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName 'Open FTP1' -Direction Inbound -LocalPort 21 -Protocol TCP -Action Allow
+Get-NetTCPConnection -State Listen
