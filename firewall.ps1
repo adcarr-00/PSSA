c49db38a2a1624ca84e7f909f36c20a4c5dd8952
adcarr-00 <adam.carr@nationwide.co.uk>
2024-11-25 11:20:10 +0000

    Update firewall.ps1

--- firewall.ps1
+++ firewall.ps1
@@ -1,5 +1,5 @@
 Write-Output 'This is a Test to check that the PS script does not open vulnerable ports inbound'
-New-NetFirewallRule -DisplayName 'Open Telnet' -Direction Inbound -LocalPort 23 -Protocol TCP -Action Allow
-New-NetFirewallRule -DisplayName 'Open FTP' -Direction Inbound -LocalPort 20 -Protocol TCP -Action Allow
-New-NetFirewallRule -DisplayName 'Open FTP1' -Direction Inbound -LocalPort 445 -Protocol TCP -Action Allow
+New-NetFirewallRule -DisplayName 'Open Telnet' -Direction Inbound -LocalPort 21001 -Protocol TCP -Service "TlntSvr" -Action Allow
+New-NetFirewallRule -DisplayName 'Open FTPD' -Direction Inbound -Protocol TCP -Service "ftpsvc" -Action Allow
+New-NetFirewallRule -DisplayName 'Open SMB' -Direction Inbound -Protocol TCP -Service "LanmanServer" -Action Allow
 Get-NetTCPConnection -State Listen
