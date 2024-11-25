e183802bc76a695253e2cb4597f37ae6e1c1bcba
adcarr-00 <adam.carr@nationwide.co.uk>
2024-11-25 11:51:42 +0000

    Update VulnerablePortsRule.psm1

--- security-settings/VulnerablePortsRule.psm1
+++ security-settings/VulnerablePortsRule.psm1
@@ -3,33 +3,24 @@
 <#
 .SYNOPSIS
 Detects the use of services with known vulnerabilities (e.g., SMB, Telnet, FTP) in firewall rule configurations.
-
 .DESCRIPTION
 Ports opened on Windows machines should not introduce known vulnerable services. To fix the violation of this rule, 
 remove the vulnerable service. Seek guidance for secure networking configuration if further assistance is required.
-
 .EXAMPLE
 Measure-VulnerablePortsRule -ScriptBlockAst $ScriptBlockAst
-
 This command analyzes the script block and checks if any of the vulnerable ports are being opened via 
 firewall rules.
-
 .INPUTS
 [System.Management.Automation.Language.ScriptBlockAst]
-
 The function accepts a ScriptBlockAst, which is the abstract syntax tree (AST) of a PowerShell script.
-
 .OUTPUTS
 [Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]]
-
 The function returns an array of DiagnosticRecord objects, which contain details about any violations found 
 in the script. Each violation includes a message, severity level, and location in the script.
-
 .NOTES
 This rule is intended to help identify potential security risks associated with opening vulnerable ports in 
 firewall rules.
 #>
-
 function Measure-VulnerablePortsRule {
     [CmdletBinding()]
     [OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord[]])]
@@ -39,14 +30,12 @@ function Measure-VulnerablePortsRule {
         [System.Management.Automation.Language.ScriptBlockAst]
         $ScriptBlockAst
     )
-
     process {
         $results = @()
         try
         {
             # Define the known vulnerable ports
-            [string[]]$vulnerableServices = @("ftpsvc", "TlntSvr", "LanmanServer")
-
+            [string[]]$vulnerableServices = @('"ftpsvc"', '"TlntSvr"', '"LanmanServer"')
             # Predicate to detect `New-NetFirewallRule` cmdlet and check for vulnerable ports
             [ScriptBlock]$Predicate = {
                 Param ([System.Management.Automation.Language.Ast]$Ast)
@@ -94,6 +83,5 @@ function Measure-VulnerablePortsRule {
         }
     }
 }
-
 # Export the function so it can be used by PSScriptAnalyzer
 Export-ModuleMember -Function Measure-*
